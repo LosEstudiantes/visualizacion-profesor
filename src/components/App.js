f6dc2f62313612d2bb67f641fad9e2ad07d88b27
Fabio <fa.espinosa10@uniandes.edu.co>
2017-12-29 13:37:59 -0500

    aclaracion

--- src/components/App.js
+++ src/components/App.js
@@ -29,12 +29,15 @@ class App extends Component {
 
 					<div>
 						<p>
-							El objetivo de las estadísticas es que los estudiantes puedan ver fácilmente qué profesores
-							son exigentes (y por lo tanto no se llevan el mejor promedio en calificaciones) pero la
-							forma de la distribución es definida (aquellos que lograron su nivel de exigencia lo
-							califican bien y aquellos que les fue mal en la materia o no lograron el nivel de exigencia
-							del profesor, lo suelen calificar mal) Entonces tenemos una distribución en forma de
-							campana invertida
+							El objetivo de las estadísticas es darle merito a aquellos profesores que son exigentes y
+							por lo tanto no se suelen llevar las mejores calificaciones (los estudiantes vagos los
+							suelen calificar mal).
+						</p>
+						<p>
+							La forma de reconocer a este tipo de profesores es con la distribución de las
+							calificaciones. Los Estudiantes que son buenos y lograron el nivel del profesor lo suelen
+							calificar bien. Los vagos lo suelen calificar mal. De esta forma tenemos una distribución
+							de campana invertida, que es lo que se espera mostrar en las estadísticas.
 						</p>
 					</div>
 					<button onClick={this.props.showEstadisticas.bind(null, 'profesor1')}>
